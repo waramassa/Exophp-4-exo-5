2326935ff6fb3a7aa59d16f60bd0933387735c5b
Wara Massa <waramassa70@gmail.com>
2020-11-24 23:58:44 +0100

    php4 exo5

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP4 exo5</title>
+</head>
+<body>
+     <?php
+     for ($number = 0; $number <= 300; $number++) {
+   if ($number % 2 != 0){
+     echo $number. ' ' ;
+}
+} ?>
+</p>
+ 
+
+</body>
+</html>
